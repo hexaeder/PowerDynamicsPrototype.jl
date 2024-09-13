9d170cb2ebd1401a90e7cde75f96fd71b6f77f8d
Documenter.jl <documenter@juliadocs.github.io>
2024-09-13 14:14:01 +0000

    build based on 9cde927

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OpPoDyn","category":"page"},{"location":"#OpPoDyn","page":"Home","title":"OpPoDyn","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OpPoDyn.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OpPoDyn]","category":"page"}]
+}
